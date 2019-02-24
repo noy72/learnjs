9003f42b62da563e80c9a73fb5619e235f120153
anoy <anoy@har.local>
2019-02-24 17:30:33 +0900

    Add login button with Google account

--- public/app.js
+++ public/app.js
@@ -110,3 +110,7 @@ learnjs.triggerEvent = function(name, args) {
 learnjs.template = function(name) {
 	return $('.templates .' + name).clone();
 }
+
+function googleSignIn() {
+	console.log(arguments);
+}
