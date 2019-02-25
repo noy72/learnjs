abf35297ac7d23c4a609634cbbd3a46f2b1dae4b
anoy <kuma.tola3@gmail.com>
2019-02-25 18:03:12 +0900

    Add a testcase for when finish all problems

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -32,6 +32,11 @@ describe('LearnJS', function() {
 		expect(elem.fadeOut).toHaveBeenCalled();
 		expect(elem.fadeIn).toHaveBeenCalled();
 	});
+	it('can redirect to the main view after the last problem is answered', function() {
+		var flash = learnjs.buildCorrectFlash(2);
+		expect(flash.find('a').attr('href')).toEqual("");
+		expect(flash.find('a').text()).toEqual("You're Finished!");
+	});
 	
 	describe('problem view', function() {
 		var view;
